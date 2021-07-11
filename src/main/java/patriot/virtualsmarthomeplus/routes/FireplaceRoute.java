ece715afacbdcc2b7a9cbaf056d0a69d28845ef7
Filip Wojnar <fwojnar@redhat.com>
2021-07-11 18:46:31 +0200

    JSON tests
    Adding more JSON tests and adding route processor to FireplaceRoute

---
+++ src/main/java/patriot/virtualsmarthomeplus/routes/FireplaceRoute.java
@@ -0,0 +1,32 @@
+package patriot.virtualsmarthomeplus.routes;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FireplaceRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        restConfiguration().component("servlet")
+                .host("localhost").port(8080)
+                .bindingMode(RestBindingMode.auto);
+
+        rest("/fireplace")
+                .get("/")
+                .produces("application/json")
+                .route().routeId("fireplace-route")
+                .process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        exchange.getMessage().setBody("{\"label\": \"fireplace\"}");
+                    }
+                })
+                .endRest();
+
+    }
+
+}
---
+++ src/test/java/patriot/virtualsmarthomeplus/JSONTests.java
@@ -0,0 +1,59 @@
+package patriot.virtualsmarthomeplus;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.apache.camel.test.spring.CamelSpringBootRunner;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.number.OrderingComparison.lessThan;
+
+@RunWith(CamelSpringBootRunner.class)
+@SpringBootTest(classes = InitialApplication.class)
+class JSONTests {
+
+    @Test
+    void AcceptingJSONGet() {
+        given()
+                .contentType(ContentType.JSON)
+                .port(8080)
+                .when().get("http://localhost:8080/fireplace")
+                .then().statusCode(200);
+    }
+
+    @Test
+    void JSONResponse() {
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .port(8080)
+                .when().get("http://localhost:8080/fireplace")
+                .then().contentType(ContentType.JSON).statusCode(200)
+                .extract().response();
+
+        String jsonAsString = response.asString();
+        assertThat(jsonAsString, equalTo("{\"label\": \"fireplace\"}"));
+    }
+
+    @Test
+    void AcceptingJSONPost() {
+        given()
+                .contentType(ContentType.JSON)
+                .port(8080)
+                .when().post("http://localhost:8080/fireplace")
+                .then().statusCode(200);
+    }
+
+    @Test
+    public void getResponseTime() {
+        SpringApplication.run(InitialApplication.class);
+        given()
+                .when().get("http://localhost:8080/fireplace")
+                .then().statusCode(200).time(lessThan(1000L));
+    }
+
+}
